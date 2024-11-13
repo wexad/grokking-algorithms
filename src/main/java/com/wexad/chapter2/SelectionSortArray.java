0a17b8e555139360b6901da3688f07d83492774f
wexad <sherzodchoriyev747@gmail.com>
2024-11-13 12:07:26 +0500

    I wrote quick sort algorithm

--- src/main/java/com/wexad/chapter2/SelectionSortArray.java
+++ src/main/java/com/wexad/chapter2/SelectionSortArray.java
@@ -10,14 +10,16 @@ public class SelectionSortArray {
     }
 
     public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int curr = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = curr;
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
     }
 }
---
+++ src/main/java/com/wexad/chapter4/QuickSort.java
@@ -0,0 +1,36 @@
+package com.wexad.chapter4;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] ints = {4, 2, 5, 3};
+        quickSort(ints, 0, ints.length - 1);
+        System.out.println(Arrays.toString(ints));
+
+    }
+
+    public static void quickSort(int[] ints, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partion(ints, low, high);
+            quickSort(ints, low, pivotIndex - 1);
+            quickSort(ints, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partion(int[] ints, int low, int high) {
+        int i = low - 1;
+        int pivot = ints[high];
+        for (int j = low; j < high; j++) {
+            if (pivot > ints[j]) {
+                i++;
+                int curr = ints[j];
+                ints[j] = ints[i];
+                ints[i] = curr;
+            }
+        }
+        ints[high] = ints[i + 1];
+        ints[i + 1] = pivot;
+        return i + 1;
+    }
+}
